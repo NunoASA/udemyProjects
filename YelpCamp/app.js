925b3bc0b1c049baa51cde2fc94eb1d3cd372c53
NunoAlmeida <nunoalcada.sa@gmail.com>
2018-11-01 16:27:10 +0000

    Add comments and Authentication

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,32 +1,44 @@
-var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose"),
-    Campground  = require("./models/campground"),
-    Comment     = require("./models/comment"),
-    seedDB      = require("./seeds")
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose"),
+    passport        = require("passport"),
+    LocalStrategy   = require("passport-local"),
+    Campground      = require("./models/campground"),
+    Comment         = require("./models/comment"),
+    User            = require("./models/user"),
+    seedDB          = require("./seeds")
     
     
-seedDB();
+    
+
 mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true, useFindAndModify: false});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
+app.use(express.static(__dirname + "/public"))
+seedDB();
 
 
-// Campground.create(
-//     {
-//         name: "Lepe Beach Campsite, Hampshire",
-//         image: "https://www.telegraph.co.uk/content/dam/Travel/2017/May/camping-Lepe-Beach.jpg?imwidth=960",
-//         description: "If a waterside campsite floats your boat, Lepe Beach is tough to top. Yards from the sandand shingle Hampshire coast, this brand new campsite offers salty sea views of the Isle of Wight and, to the east, the sand flats of the Solent National Nature Reserve. The focus here is on traditional camping: pitches are flat, ablutions kept immaculately clean and campfiresare welcomed in the pits available." 
-        
-//     }, function(err, campground){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             console.log("Newly created Campground: ");
-//             console.log(campground);
-//         }
-//     });
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret:"WorldHELLO",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Midleware
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    next()
+})
+
+
+
 
 
 app.get("/", function(req, res){
@@ -41,7 +53,7 @@ app.get("/campgrounds", function(req,res){
       if(err){
         console.log(err);
       } else {
-        res.render("index", {campgrounds: allcampgrounds});
+        res.render("campgrounds/index", {campgrounds: allcampgrounds, currentUser: req.user});
       }
     })
 })
@@ -68,7 +80,7 @@ app.post("/campgrounds", function(req, res){
 
 // NEW
 app.get("/campgrounds/new", function(req, res) {
-    res.render("new");
+    res.render("campgrounds/new");
 })
 
 
@@ -78,13 +90,98 @@ app.get("/campgrounds/:id", function(req, res) {
         if (err){
             console.log(err) 
         } else {
-            console.log(foundCampground)
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     })
   
 })
 
+
+// =======================================
+// COMMENTS ROUTES
+// =======================================
+
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err,campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    })
+})
+
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res){
+    // lookup campground using ID
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err)
+            res.redirect("/campgrounds");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + campground._id);
+                }
+            })
+        }
+    })
+    // create new comment
+    // connect new comment to campground
+    // redirect campground show page
+})
+
+
+//================
+//AUTH ROUTES
+//================
+
+//show register from
+app.get("/register", function(req, res){
+    res.render("register");
+})
+//handle sigh up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username})
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err)
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        });
+    })
+})
+
+// Show login form
+app.get("/login", function(req,res){
+    res.render("login")
+})
+
+// handling login logic
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/campgrounds",
+    failureRedirect: "/login"
+}), function(req, res){});
+
+
+//Logout route
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/campgrounds")
+})
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!");
